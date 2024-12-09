b848f2e23a2c1af24c9d52668ddde9fddd0221a1
kai <137367860+Itzsoham@users.noreply.github.com>
2024-12-09 18:13:39 +0530

    final commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -82,10 +82,10 @@ export default async function Page() {
 
           <div>
             <a
-              href="/cabins"
+              href="https://royalstay.vercel.app/"
               className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
             >
-              Explore our luxury cabins
+              Explore our admin panel
             </a>
           </div>
         </div>
